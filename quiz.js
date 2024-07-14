316f86aa5077d94b5cb773eeee3b41bff81e0b0d
Marilyn Adjei <adjeimarilyn22@gmail.com>
2024-07-14 17:13:51 +0000

    initial commit

---
+++ quiz.js
@@ -0,0 +1,15 @@
+function checkAnswer () {
+    const correctAnswer = '4';
+    const selectedOption = document.querySelector('input[name="quiz"]:checked');
+    const userAnswer = selectedOption ? selectedOption.value : null;
+
+    const feedbackElement = document.getElementById('feedback');
+
+    if(userAnswer==correctAnswer) {
+        feedbackElement.textContent = 'Correct Answer. Good Job!'
+    } else {
+        feedbackElement.textContent = 'Wrong Answer. Try again'
+    };
+}
+
+document.getElementById('submit-answer').addEventListener('click', checkAnswer);
